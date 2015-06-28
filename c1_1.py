f679cf28521d98e6204192f467707389bc09b6a2
isavin <caba1983@gmail.com>
2015-06-28 23:53:45 +0300

    some tasks solved

---
+++ c1_1.py
@@ -0,0 +1,33 @@
+#coding: utf8
+def power_optimized(a,n):
+    """
+    1.1.4. Посчитать а в степени n за logn операций
+    """
+    b=1
+    c=a
+    k=n
+    for i in range(n):
+        if k%2==0:
+            c=c*c
+            k=k/2
+        else:
+            b=c*b
+            k=k-1
+
+        if k==0:
+            return b
+    return b
+
+def div_mod(a,b):
+    """
+    1.1.7. Дано два числа, вычислить остаток и частное, не используя div и mod
+    """
+    c,d=a,b
+    mod1=0
+    div1=0
+    while (c>d):
+        div1=div1+1;
+        c=c-d
+    return (div1,c)
+
+print(div_mod(17,5))
